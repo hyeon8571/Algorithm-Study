f3c419a01d21690857c258f1b101d59bbd4560a0
Seunghyeon Won <w01085914442@gmail.com>
2022-04-02 00:36:17 +0900

    Algorithm Study

--- src/selection/AvgSelect.java
+++ src/selection/AvgSelect.java
@@ -1,31 +1,56 @@
 package selection;
-
+/*
+    평균 선형시간 선택 알고리즘
+    시간복잡도: 평균 O(n), 최악의 경우 O(n^2)
+    퀵정렬 유사하지만 단순히 작은 값은 왼쪽 큰 값은 오른쪽으로만 보냄
+ */
 public class AvgSelect {
+    public static int select(int[] arr, int p, int r, int target) {
 
+        if (p == r)
+            return arr[p];  //배열의 요소가 한개인 경우이므로 바로 출력
+
+        int q = partition(arr, p, r);
+
+        int k = q - p + 1; //pivot이 몇번째 작은원소인지
+
+        if (target < k)
+
+            return select(arr, p, q - 1, target);
+
+        else if (target == k)
+
+            return arr[q];
+
+        else
+            return select(arr, q + 1, r, target - k);//pivot까지의 값을 제외하고 작은 값을 찾아야 하므로 target - k 번째 작은 값을 찾아야함
+
+    }
     public static int partition(int[] arr, int p, int r) {
 
-        int x = arr[r]; //좌우 분할 기준값
-
+        int pivot = arr[r]; //좌우 분할 기준값
+/*
+    만약 i = p로 할 경우 i++를 해줘야 한다. 이 때 if문이 끝났을 때 i값이 자동을 1 증가하므로
+    pivot위치를 옮겨줄 때 arr[i+1]이 아닌 arr[i]를 해줘야 한다. 이 경우 이해하는데 있어
+    문제가 될 수 있으므로 i를 p-1로 초기화 해준다.
+ */
         int i = p - 1;
 
         int temp;
 
         for (int n = p; n < r; n++) {
 
-            if (arr[n] <= x) {//더 작은 값을 좌측으로
+            if (arr[n] <= pivot) { //pivot보다 작은값은 왼쪽에 배치
 
                 temp = arr[n];
 
                 arr[n] = arr[++i];
 
                 arr[i] = temp;
-
             }
-
         }
 
-//더 작은 값들을 i까지의 인덱스에 보내주었다. i+1의 인덱스에 해당하는 value는 무조건 A[r]보다 큰 값이니 A[r]과 A[i+1]을 swap한다.
-
+        //pivot을 기준으로 더 작은값들은 배열을 i번째 까지 배치 되었으므로 pivit을 i+1번째로 옮겨준다.
         temp = arr[r];
 
         arr[r] = arr[i + 1];
@@ -37,36 +62,11 @@ public class AvgSelect {
     }
 
 
-    public static int select(int[] arr, int p, int r, int i) {
-
-        if (p == r)
-
-            return arr[p];
-
-
-        int q = partition(arr, p, r);
-
-        int k = q - p + 1;
-
-        if (i < k)
-
-            return select(arr, p, q - 1, i);
-
-        else if (i == k)
-
-            return arr[q];
-
-        else
-            return select(arr, q + 1, r, i - k);//여기서 i번째로 작은 원소는 이미 K개 만큼의 자신보다 더 작은 원소를 가지고 있기 때문에 마지막 인수에 i가 아닌 i-k를 사용한다.
-
-    }
-
-
     public static void main(String[] args) {
 
         int[] arr = new int[] {7, 15, 3, 28, 25, 17, 5};
 
-        int target = select(arr, 0, arr.length -1, 3);
+        int target = select(arr, 0, arr.length -1, 5);
         System.out.println(target);
     }
 
